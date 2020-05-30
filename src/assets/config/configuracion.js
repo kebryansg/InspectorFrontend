bbf1610093dae249feefaaecddc3295d6a3594f3
kebryansg <kebryansg@gmail.com>
2020-05-30 02:44:20 -0500

    Modificacion URLS

--- src/assets/config/configuracion.js
+++ src/assets/config/configuracion.js
@@ -1,5 +1,7 @@
 var configuracion = {
-  url: 'http://localhost/api/public/',
-  // url: 'http://inspector.api.buhocorp.com/',
+  // LARAVEL SERVER
+  // url: 'http://localhost:8000/',
+  // XAMPP
+  url: 'http://localhost/API/PoluxInspector.BuenaFe/public/',
   fcm:'AAAAXY--lH0:APA91bFYpA8o-ULCLl2_r8Ig7HjFQua-t3YXyBERYhF8YXF7PO_SL8sDzKKMOmnY_gtvc4v2b06BfKR4Ql4QZdoWHgzH0ki0148B7AOeTPBT4Osbj3omPWPO9HEHphBWrLa0IZXCNlzm'
 }
