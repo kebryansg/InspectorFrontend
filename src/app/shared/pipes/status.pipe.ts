f83ad759855936b8510cfa52cc6d27f2847e85e6
kebryansg <kebryansg@gmail.com>
2020-05-30 02:44:05 -0500

    Update status.pipe.ts

--- src/app/shared/pipes/status.pipe.ts
+++ src/app/shared/pipes/status.pipe.ts
@@ -1,26 +1,26 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
   name: 'status'
 })
 export class StatusPipe implements PipeTransform {
 
+  case = {
+    'ACT': 'Activo',
+    'INA': 'Inactivo',
+    'ANU': 'Anulado',
+    'PEN': 'Pendiente',
+    'APR': 'Aprobado',
+    'REP': 'Reprobado',
+    'P': 'Persona',
+    'E': 'Empresa',
+    'S': 'Si',
+    'N': 'No',
+    'A': 'No',
+  };
+
   transform(value: any, args?: any): any {
-    let result = '';
-    switch (value) {
-      case 'ACT': result = 'Activo';  break;
-      case 'INA': result = 'Inactivo';  break;
-      case 'ANU': result = 'Anulado';  break;
-      case 'PEN': result = 'Pendiente';  break;
-      case 'APR': result = 'Aprobado';  break;
-      case 'REP': result = 'Reprobado';  break;
-      case 'P': result = 'Persona';  break;
-      case 'E': result = 'Empresa';  break;
-      case 'S': result = 'Si';  break;
-      case 'N': result = 'No';  break;
-      case 'A': result = 'No Aplica';  break;
-    }
-    return result;
+    return this.case[value];
   }
 
 }
